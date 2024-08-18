878b2de5b3817a89b08797b17c4be65f00c63102
Michael Orchard <michaelorchard@Michaels-MacBook-Pro-2.local>
2024-08-18 15:45:54 +0100

    Add station template. Get editor buttons rendering

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 [RequireComponent(typeof (TrackPieceController))]
 public class Station : MonoBehaviour {
+    public TrackTemplate stationTemplate;
+
     void Awake() {
+        TrackPieceController trackPieceController = GetComponent<TrackPieceController>();
+        trackPieceController.TrackPiece = new TrackPiece() { X = 0, Y = 0, Template = stationTemplate };
+
         TrackPiecePlacementButtons trackPiecePlacementButtons = GetComponentInChildren<TrackPiecePlacementButtons>();
         if (trackPiecePlacementButtons != null) {
             trackPiecePlacementButtons.OnClick.AddListener(OnClickPlacement);
