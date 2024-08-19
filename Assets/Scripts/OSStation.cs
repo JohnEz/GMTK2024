49112ab357ea955f2eb4f3a4a3fea4de6160d73e
Andrew Jones <andrew_nomie@msn.com>
2024-08-19 22:24:20 +0100

    update spawning

--- Assets/Scripts/OSStation.cs
+++ Assets/Scripts/OSStation.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class OSStation : MonoBehaviour {
     private const int MAX_PASSENGERS = 20;
+    private const float PASSENGER_SPAWN_DELAY = 5f;
+    private const float PASSENGER_SPAWN_DELAY_DECAY = .1f;
+    private const float MIN_PASSENGER_SPAWN_DELAY = .2f;
 
     public List<OSPassenger> Passengers { get; private set; } = new List<OSPassenger>();
 
@@ -16,6 +19,9 @@ public class OSStation : MonoBehaviour {
     [SerializeField]
     private TMP_Text passengerCountText;
 
+    private float _currentSpawnDelay;
+    private float _timeSincePassengerSpawn = 0f;
+
     private void Awake() {
         TrackPieceController = GetComponent<TrackPieceController>();
     }
@@ -24,7 +30,20 @@ public class OSStation : MonoBehaviour {
         gameObject.name = name;
         titleText.text = name;
 
+        _currentSpawnDelay = PASSENGER_SPAWN_DELAY;
+        _timeSincePassengerSpawn = Random.Range(0, _currentSpawnDelay);
+
         UpdatePassengerCount();
+    }
+
+    private void Update() {
+        _timeSincePassengerSpawn += Time.deltaTime;
+        if (_timeSincePassengerSpawn > _currentSpawnDelay) {
+            PassengerManager.Instance.SpawnPassenger(this);
+            _timeSincePassengerSpawn -= _currentSpawnDelay;
+
+            _currentSpawnDelay = Mathf.Max(_currentSpawnDelay - PASSENGER_SPAWN_DELAY_DECAY, MIN_PASSENGER_SPAWN_DELAY);
+        }
     }
 
     public void AddPassenger(OSPassenger newPassenger) {
