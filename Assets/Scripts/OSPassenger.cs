61a7ddebffbc0381612a970084421d3fa4e9cf07
Andrew Jones <andrew_nomie@msn.com>
2024-08-19 16:37:44 +0100

    Add path finding

--- Assets/Scripts/OSPassenger.cs
+++ Assets/Scripts/OSPassenger.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OSPassenger : MonoBehaviour {
-    public OSStation TargetStation { get; private set; }
+    public OSStation CurrentStation { get; private set; }
+
+    public OSStation FinalStation { get; private set; }
 
     public PassengerPath Path { get; private set; }
+
+    public OSStation StationToGetOffAt { get; private set; }
+
+    private bool isRidingTrain = false;
+
+    public void OnDestroy() {
+        OSConnectionManager.Instance.OnConnectionsChange -= HandleConnectionsChange;
+    }
 
     public void Setup(OSStation startingStation) {
         // kill self?
 
         CalculateRandomEndStation(startingStation);
 
-        //Debug.Log($"I spawned at {startingStation.name} and i want to go to {TargetStation.name}");
+        CurrentStation = startingStation;
 
-        CalculatePath();
+        HandleConnectionsChange();
+        OSConnectionManager.Instance.OnConnectionsChange += HandleConnectionsChange;
+
+        Debug.Log($"I spawned at {startingStation.name} and I want to go to {FinalStation.name}. It will be {Path.Connections.Count} stops.");
     }
 
     private void CalculateRandomEndStation(OSStation startingStation) {
@@ -28,10 +41,25 @@ public class OSPassenger : MonoBehaviour {
             targetStation = OSMapManager.Instance.Stations[targetIndex];
         } while (targetStation == startingStation && iterations < 1000);
 
-        TargetStation = targetStation;
+        FinalStation = targetStation;
     }
 
-    private void CalculatePath() {
-        Path = new PassengerPath();
+    public void GetOnTrain() {
+        isRidingTrain = true;
+        StationToGetOffAt = Path.Connections[0].Station;
+        Path.Connections.RemoveAt(0);
+    }
+
+    public void ArriveAtStation(OSStation newStation) {
+        isRidingTrain = false;
+        CurrentStation = newStation;
+    }
+
+    public bool ShouldGetOnTrain(Color lineColor) {
+        return lineColor == Path.Connections[0].LineColor;
+    }
+
+    private void HandleConnectionsChange() {
+        Path = OSConnectionManager.Instance.CalculatePath(isRidingTrain ? StationToGetOffAt : CurrentStation, FinalStation);
     }
 }
--- Assets/Scripts/Singletons/OSConnectionManager.cs
+++ Assets/Scripts/Singletons/OSConnectionManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
+using UnityEditor.MemoryProfiler;
 using UnityEngine;
 
 public class StationConnection {
@@ -19,16 +21,34 @@ public class PassengerPath {
     public OSStation Start;
     public OSStation End;
 
-    public List<PassengerPathConnection> Connections = new List<PassengerPathConnection>();
+    public List<StationConnection> Connections = new List<StationConnection>();
 }
 
-public class PassengerPathConnection {
-    public OSStation NextStation;
-    public Color ColorTrainToCatch;
+public class Node {
+
+    public Node() {
+    }
+
+    public Node(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+
+    public int x = -1;
+    public int y = -1;
+
+    public int cost = 0;
+    public int dist = 0;
+    public Node previous = null;
+
+    // bad its here?
+    public StationConnection connection = null;
 }
 
-public class OSConnectionManager : MonoBehaviour {
+public class OSConnectionManager : Singleton<OSConnectionManager> {
     public Dictionary<OSStation, List<StationConnection>> ConnectionMap { get; private set; } = new();
+
+    public event Action OnConnectionsChange;
 
     private void OnEnable() {
         LineManager.Instance.OnRouteLineChange += GenerateConnectionMap;
@@ -71,6 +91,12 @@ public class OSConnectionManager : MonoBehaviour {
         //}
 
         //Debug.Log("---Connections End----");
+
+        OnConnectionsChange?.Invoke();
+    }
+
+    public static int ManhattanDistance(int x1, int x2, int y1, int y2) {
+        return Mathf.Abs(x1 - x2) + Mathf.Abs(y1 - y2);
     }
 
     public PassengerPath CalculatePath(OSStation start, OSStation end) {
@@ -78,6 +104,106 @@ public class OSConnectionManager : MonoBehaviour {
         path.Start = start;
         path.End = end;
         path.Connections = new();
+
+        if (start == null || end == null || ConnectionMap.Count == 0) {
+            return path;
+        }
+
+        Dictionary<OSStation, Node> nodes = new();
+
+        List<Node> openList = new();
+
+        Node source = new Node(start.TrackPieceController.TrackPiece.X, start.TrackPieceController.TrackPiece.Y);
+        Node target = new Node(end.TrackPieceController.TrackPiece.X, end.TrackPieceController.TrackPiece.Y);
+
+        nodes.Add(start, source);
+        nodes.Add(end, target);
+
+        nodes.Values.ToList().ForEach(key => {
+            key.cost = int.MaxValue;
+            key.previous = null;
+            key.dist = ManhattanDistance(key.x, target.x, key.y, target.y);
+        });
+
+        source.cost = 0;
+        source.previous = null;
+        source.dist = 0;
+
+        openList.Add(source);
+
+        while (openList.Count > 0) {
+            // find the current lowest cost node
+            Node u = null;
+            foreach (Node node in openList) {
+                if (u == null || (node.cost + node.dist) < (u.cost + u.dist) || node == source || node == target) {
+                    u = node;
+                }
+            }
+
+            if (u == target || u == null) {
+                break; //either no path or full path found
+            }
+
+            openList.Remove(u);
+
+            OSStation currentStation = nodes.Where(kvp => kvp.Value == u).FirstOrDefault().Key;
+
+            if (currentStation == null) {
+                continue;
+            }
+
+            ConnectionMap[currentStation].ForEach(connection => {
+                OSStation neighbour = connection.Station;
+                bool nodeExists = nodes.ContainsKey(neighbour);
+                Node neighbourNode;
+                if (nodeExists) {
+                    neighbourNode = nodes[neighbour];
+                } else {
+                    neighbourNode = new Node(neighbour.TrackPieceController.TrackPiece.X, neighbour.TrackPieceController.TrackPiece.Y);
+                    nodes.Add(neighbour, neighbourNode);
+                }
+
+                int newCost = u.cost + 1;
+
+                //if new cost is less than current cost or it isnt in dictionary
+                if (!nodeExists || newCost < neighbourNode.cost) {
+                    neighbourNode.cost = newCost;
+                    neighbourNode.previous = u;
+                    neighbourNode.connection = connection;
+                    openList.Add(neighbourNode);
+                }
+            });
+        }
+
+        // Debug.Log($"-Generating path from: {start.name} to {end.name}-");
+
+        if (target.previous == null) {
+            // no path was found
+            // Debug.Log("-----No Path Found------");
+            return path;
+        }
+
+        List<Node> currentPath = new();
+        Node currentNode = target;
+
+        while (currentNode != null) {
+            if (currentNode != source) {
+                currentPath.Add(currentNode);
+                path.Connections.Add(currentNode.connection);
+            }
+            currentNode = currentNode.previous;
+        }
+
+        currentPath.Reverse();
+        path.Connections.Reverse();
+
+        //Debug.Log("-----Path------");
+
+        //path.Connections.ForEach(connection => {
+        //    Debug.Log($"Take the {connection.LineColor} line to {connection.Station.name}");
+        //});
+
+        //Debug.Log("-----Path Complete------");
 
         return path;
     }
