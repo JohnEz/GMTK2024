8b5971aaa2f5482636a63153b7da493b20daefb4
Jamie Macdonald <jamie.whit.mac@gmail.com>
2024-08-20 14:56:26 +0100

    Add more tutorials

--- Assets/Scripts/Singletons/TutorialManager.cs
+++ Assets/Scripts/Singletons/TutorialManager.cs
@@ -20,12 +20,33 @@ public class TutorialManager : Singleton<TutorialManager>
     public string returnToKidTutorial;
 
     [TextAreaAttribute]
+    public string connectStationTutorial;
+
+    [TextAreaAttribute]
+    public string returnToAdultTutorial;
+
+    [TextAreaAttribute]
+    public string secondLineTutorial;
+
+    [TextAreaAttribute]
+    public string changeLineTutorial;
+
+    [TextAreaAttribute]
     public string coolPointsTutorial;
+
+    [TextAreaAttribute]
+    public string moneyTutorial;
+
+    [TextAreaAttribute]
+    public string addTrainsTutorial;
+
+    private Route _firstRoute;
 
     // Start is called before the first frame update
     void Start()
     {
         DisplayIntroduction();
+        PassengerManager.Instance.PauseSpawning();
         GameStateManager.Instance.OnStateChange += HandleStateChange;
     }
 
@@ -57,6 +78,7 @@ public class TutorialManager : Singleton<TutorialManager>
     }
 
     void DisplayScaleUpTutorial(Route route) {
+        _firstRoute = route;
         RouteManager.Instance.OnRouteAdded -= DisplayScaleUpTutorial;
         DisplayDialog(Speaker.Lenny, scaleUpTutorial);
         GameStateManager.Instance.OnStateChange += DisplayLineTutorial;
@@ -72,8 +94,53 @@ public class TutorialManager : Singleton<TutorialManager>
 
     void DisplayReturnToKidTutorial(Color color, Line line) {
         LineManager.Instance.OnLineAdded -= DisplayReturnToKidTutorial;
-        PresetStationScheduler.Instance.StartSpawning();      
+        PresetStationScheduler.Instance.StartSpawning();
+        StationManager.Instance.OnStationAdded += StopStationsSpawning;      
         DisplayDialog(Speaker.Terrence, returnToKidTutorial);
+        GameStateManager.Instance.OnStateChange += ConnectStationTutorial;
+    }
+
+    void StopStationsSpawning(TrackPiece track) {
+        StationManager.Instance.OnStationAdded -= StopStationsSpawning;     
+        PresetStationScheduler.Instance.Pause();
+    }
+
+    void ConnectStationTutorial(GameState state) {
+        if (state == GameState.Kid) {
+            GameStateManager.Instance.OnStateChange -= ConnectStationTutorial;
+            BankManager.Instance.Spend(-1);
+            DisplayDialog(Speaker.Lenny, connectStationTutorial);
+            RouteManager.Instance.OnRouteAdded += ReturnToAdultTutorial;
+        }
+    }
+
+    void ReturnToAdultTutorial(Route route) {
+        RouteManager.Instance.OnRouteAdded -= ReturnToAdultTutorial;
+        DisplayDialog(Speaker.Lenny, returnToAdultTutorial);
+        GameStateManager.Instance.OnStateChange += SecondLineTutorial;
+    }
+
+    void SecondLineTutorial(GameState state) {
+        if (state == GameState.Adult) {
+            // GAME STARTS
+            PassengerManager.Instance.ResumeSpawning();
+            PresetStationScheduler.Instance.Resume();
+
+            GameStateManager.Instance.OnStateChange -= SecondLineTutorial;
+            DisplayDialog(Speaker.Terrence, secondLineTutorial);
+            LineManager.Instance.OnRouteLineChange += ChangeLineTutorial;
+        }
+    }
+
+    void ChangeLineTutorial() {
+            LineManager.Instance.OnRouteLineChange -= ChangeLineTutorial;
+            DisplayDialog(Speaker.Terrence, changeLineTutorial);
+            LineManager.Instance.OnRouteLineChange += Break;
+    }
+
+    void Break() {
+        LineManager.Instance.OnRouteLineChange -= Break;
+        HideDialog();
         GameStateManager.Instance.OnStateChange += DisplayCoolPointsTutorial;
     }
 
@@ -81,12 +148,20 @@ public class TutorialManager : Singleton<TutorialManager>
         if (state == GameState.Kid) {
             GameStateManager.Instance.OnStateChange -= DisplayCoolPointsTutorial;
             DisplayDialog(Speaker.Lenny, coolPointsTutorial);
+            RouteManager.Instance.OnRouteAdded += DisplayMoneyTutorial;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void DisplayMoneyTutorial(Route route) {
+        RouteManager.Instance.OnRouteAdded -= DisplayMoneyTutorial;
+        DisplayDialog(Speaker.Lenny, moneyTutorial);
+        GameStateManager.Instance.OnStateChange += DisplayAddTrainsTutorial;
+    }
+
+    void DisplayAddTrainsTutorial(GameState state) {
+        if (state == GameState.Adult) {
+            GameStateManager.Instance.OnStateChange -= DisplayAddTrainsTutorial;
+            DisplayDialog(Speaker.Terrence, addTrainsTutorial);
+        }
     }
 }
