4e07e5f5af0468a2a3c3904619ebbea9d657ae86
Andrew Jones <andrew_nomie@msn.com>
2024-08-20 17:41:51 +0100

    group cash

--- Assets/Scripts/Singletons/PassengerManager.cs
+++ Assets/Scripts/Singletons/PassengerManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PassengerManager : Singleton<PassengerManager> {
@@ -9,9 +10,13 @@ public class PassengerManager : Singleton<PassengerManager> {
 
     public List<OSPassenger> PassengerList { get; private set; } = new List<OSPassenger>();
 
-    private float timePassed = 0;
+    private float _moneyFlushTimer = 0.1f;
+
+    private float timePassedSincePassengers = 0;
 
     private bool _isSpawningAllowed = true;
+
+    private Dictionary<OSStation, decimal> _moneyToAdd = new();
 
     public int CompletedJournyCount {
         get;
@@ -19,12 +24,18 @@ public class PassengerManager : Singleton<PassengerManager> {
     } = 0;
 
     private void Update() {
-        //timePassed += Time.deltaTime;
-        //if (timePassed > 1f) {
-        //    OSStation station = OSMapManager.Instance.Stations[Random.Range(0, OSMapManager.Instance.Stations.Count)];
-        //    SpawnPassenger(station);
-        //    timePassed = 0f;
-        //}
+        timePassedSincePassengers += Time.deltaTime;
+        if (timePassedSincePassengers > _moneyFlushTimer) {
+            timePassedSincePassengers -= _moneyFlushTimer;
+
+            _moneyToAdd.Keys.ToList().ForEach(key => {
+                if (_moneyToAdd[key] > 0) {
+                    WorldFloatingTextManager.Instance.Show($"+£{_moneyToAdd[key]:N0}", key.gameObject, isAdult: true);
+                }
+            });
+
+            _moneyToAdd.Clear();
+        }
     }
 
     public void ResumeSpawning() {
@@ -61,10 +72,16 @@ public class PassengerManager : Singleton<PassengerManager> {
         );
         decimal adultPrice = BankManager.Instance.GetAdultValue(routePrice);
 
-        WorldFloatingTextManager.Instance.Show($"+£{adultPrice:N0}", passenger.FinalStation.gameObject, isAdult: true);
+        //WorldFloatingTextManager.Instance.Show($"+£{adultPrice:N0}", passenger.FinalStation.gameObject, isAdult: true);
 
         if (!GameStateManager.Instance.IsGameOver()) {
             CompletedJournyCount++;
+        }
+
+        if (_moneyToAdd.ContainsKey(passenger.FinalStation)) {
+            _moneyToAdd[passenger.FinalStation] += adultPrice;
+        } else {
+            _moneyToAdd[passenger.FinalStation] = adultPrice;
         }
 
         // Debug.Log($"Passenger completed their journey from {passenger.StartingStation} to {passenger.FinalStation}.");
